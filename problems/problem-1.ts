5321c66d174c65e81d559471d80f07814d1aa18a
SadikRa <sadikrahman494@gmail.com>
2024-11-07 20:23:01 +0600

    problem one done

---
+++ problems/problem-1.ts
@@ -0,0 +1,15 @@
+{
+    // Write a TypeScript function sumArray that takes an array of numbers and returns the sum of all elements in the array.
+  
+    function sumArray(numbers: number[]): number {
+        const sum = numbers.reduce((num, currentNum) => num + currentNum, 0)
+        return sum
+    }
+    
+    // take a look 
+    const arrayOfNumbers = [12, 13, 14, 15, 16, 17, 18, 19];
+    console.log(sumArray(arrayOfNumbers))
+  
+    ///
+  }
+  
